ec03d69218538301c37f0f36daaa3c8bc90ff3a3
fabianthome <thome.fabian@web.de>
2022-12-12 14:17:19 +0100

    updated comments

--- src/calibration.py
+++ src/calibration.py
@@ -20,40 +20,41 @@ def get_camera_calibration(plot: bool = False):
     -------
         Parameters required to undistored image.
     """
-    # termination criteria
+    # termination criteria: min accuracy or max iteration
     criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 
-    # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
+    # prepare object points
     objp = np.zeros((VERTICAL_CORNERS * HORIZONTAL_CORNERS, 3), np.float32)
     objp[:, :2] = np.mgrid[0:HORIZONTAL_CORNERS, 0:VERTICAL_CORNERS].T.reshape(-1, 2)
 
-    # Arrays to store object points and image points from all the images.
-    objpoints = []  # 3d point in real world space
-    imgpoints = []  # 2d points in image plane.
+    # arrays for object and image points
+    objpoints = []  # represents points in real world space (3D)
+    imgpoints = []  # represents points in image plane (2D)
 
+    # path to calibration images
     images = glob.glob("data/img/Udacity/calib/*.jpg")
 
     for fname in images:
         img = cv2.imread(fname)
         gray: NDArray[np.uint8] = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY).astype(np.uint8)
 
-        # Find the chess board corners
+        # find corners
         ret, corners = cv2.findChessboardCorners(gray, (VERTICAL_CORNERS, HORIZONTAL_CORNERS), None)
 
-        # If found, add object points, image points (after refining them)
+        # if found, add object points and image points (after increasing their accuracy)
         if ret is True:
             objpoints.append(objp)
             corners2 = cv2.cornerSubPix(gray, corners, (11, 11), (-1, -1), criteria)
             imgpoints.append(corners2)
 
             if plot:
-                # Draw and display the corners
+                # draw detection points and lines onto image
                 cv2.drawChessboardCorners(img, (HORIZONTAL_CORNERS, VERTICAL_CORNERS), corners2, ret)
                 cv2.imshow("Corner Detection for " + fname.split("/")[-1], img)
                 cv2.waitKey(0)
     cv2.destroyAllWindows()
 
-    # retval, camera matrix, distortion coefficients, rotation vectors, translation vectors
+    # get return, camera matrix, distortion coefficients, rotation vectors and translation vectors
     ret, mtx, dist, _, _ = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
 
     return mtx, dist
