ea7b913c67c6bf483c1f151c7629f77164a91a17
nbs2904 <62153072+nbs2904@users.noreply.github.com>
2022-12-12 18:07:03 +0100

    💡 add comments

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from src.image import display_image
 from src.video import display_video
 
-VIDEO_NAME = "./data/img/Udacity/project_video.mp4"
+VIDEO_NAME = "./data/img/Udacity/challenge_video.mp4"
 IMAGE_NAME = "./data/img/KITTI/image010.jpg"
 ALL_KITTI_IMAGES = [
     "./data/img/KITTI/image009.jpg",
@@ -24,7 +24,12 @@ ALL_UDACITY_IMAGES = [
 ]
 
 if __name__ == "__main__":
+    # ? uncomment if video should be displayed
     # display_video(VIDEO_NAME)
-    # for image in ALL_KITTI_IMAGES:
-    #     display_image(image)
-    display_image(IMAGE_NAME)
+
+    # ? uncomment if all images of a dataset should be displayed
+    for image in ALL_KITTI_IMAGES:
+        display_image(image)
+
+    # ? uncomment if a single image should be displayed
+    # display_image(IMAGE_NAME)
